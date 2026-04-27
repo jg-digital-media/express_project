d95fb3fa71449cd34df02f45dfb7ea2c2e44a29c
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-04-27 14:10:17 +0100

    edit link to repo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Express Project</title>
+</head>
+<body>
+    
+    <h1>Express Project</h1>
+
+    <p>index.php.</p>
+
+    <a href="https://github.com/jg-digital-media/express_project" target="_blank">Repository</a>
+</body>
+</html>
