ffecf537fec3647a141dd31475edcbe7c5cc165e
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-04-29 15:26:26 +0100

    install express.js

---
+++ express-quote-app/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
+
+// Test a route with app.get
+app.get("/", (req, res) => {
+  res.send("Hello from your Quote App!");
+});
