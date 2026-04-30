05a7b79db9b48f6fe7a76f65c6bc014bfb5f5fa1
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-04-30 10:51:01 +0100

    build basic express quote app

--- express-quote-app/app.js
+++ express-quote-app/app.js
@@ -1,20 +1,55 @@
 const express = require("express");
 const app = express();
 
+// load data file
+const quotes = require("./data/quotes.json");
+
 // Start server
 app.listen(3000, () => {
+
   console.log("Server running on http://localhost:3000");
 });
 
 // Test a route with app.get
 app.get("/", (req, res) => {
-  res.send("Hello from your Quote App!");
+  
+    const random = quotes[Math.floor(Math.random() * quotes.length)];
+
+    res.send(`
+        <h1>Daily Quote</h1>
+        <blockquote>${random.quote}</blockquote>
+        <p>- ${random.author}</p>
+        <a href="/browse">Browse Quotes</a>
+    `);
+
 });
 
 app.get("/browse", (req, res) => {
-  res.send("Quotes list page");
+
+  let html = "<h1>All Quotes</h1>";
+
+  quotes.forEach(q => {
+    html += `<p><a href="/browse/${q.id}">${q.quote}</a></p>`;
+  });
+
+  html += `<a href="/">Back</a>`;
+
+  res.send(html);
+
 });
 
-app.get("/browse/1", (req, res) => {
-  res.send("Single quote page");
+app.get("/browse/:id", (req, res) => {
+  const quote = quotes.find(q => q.id == req.params.id);
+
+  if (!quote) {
+    return res.send("Quote not found");
+  }
+
+  res.send(`
+    <h1>Quote</h1>
+    <blockquote>${quote.quote}</blockquote>
+    <p>- ${quote.author}</p>
+    <a href="/browse">Back</a>
+  `);
+
 });
